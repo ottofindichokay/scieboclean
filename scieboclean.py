8bf0064b2b82fb1c60710b4cfcea9209d25df824
Hendrik Weber <hendrik.weber@ika.rwth-aachen.de>
2019-09-05 10:31:44 +0200

    Add first steps
    1. Walk the dir
    2. Look for backup files
    3. Check if the backup file has an original counterpart

---
+++ scieboclean.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import sys, os, re
+
+def main():
+    if len(sys.argv) == 1:
+        sciebo_dir = "D:\\sciebo\\"
+    else:
+        sciebo_dir = sys.argv[2]
+    
+    for dirpath, dirs, filenames in os.walk(sciebo_dir):
+        clean_directory(dirpath, dirs, filenames)
+
+def clean_directory(path, dirs, files):
+    backup_files = []
+    orig_files = []
+
+    for f in files:
+        if re.match(r'.*.~', f):
+            backup_files.append(f);
+        else:
+            orig_files.append(f);
+
+    #if len(backup_files) > 1: print(backup_files)
+    for f in backup_files: has_orig(f, orig_files)
+
+def has_orig(bac_file, orig_files):
+    cand_name = bac_file[1:-6]
+    orig = [f for f in orig_files if cand_name in f]
+
+    if len(orig) == 1:
+        print('Found candidate for ' + cand_name)
+    else:
+        raise Exception('Found multiple candidates') #This should not be possible to happen
+
+
+if __name__ == "__main__":
+    main()
